5af4247d3544bb25dc37b7825259407fbcfea568
jixiegit <andres.manniksaar@gmail.com>
2026-09-15 11:36:18 +0300

    Spring boot project start

---
+++ ladder-api/src/main/java/com/jxie/ladder_api/LadderApiApplication.java
@@ -0,0 +1,13 @@
+package com.jxie.ladder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadderApiApplication.class, args);
+	}
+
+}
---
+++ ladder-api/src/test/java/com/jxie/ladder_api/LadderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxie.ladder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
